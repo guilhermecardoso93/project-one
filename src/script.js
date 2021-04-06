1cf9b5cbf8617fea5933f5e818f52698cdafcc35
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2021-04-05 22:39:57 -0300

    aula-10

---
+++ src/script.js
@@ -0,0 +1,31 @@
+$(document).ready(() =>{
+    
+    const slickOptions = {
+        autoplay: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;',
+    };
+    $(".slider").slick(slickOptions);
+
+    $('.footer__form-button').on('click',() => {
+        const valeu = $('#email').vaç();
+        Email.send({
+            Host : "smtp.yourisp.com",
+            Username : "username",
+            Password : "password",
+            To : 'them@website.com',
+            From : email,
+            Subject : "Por favor, me adione na newletter",
+            Body : `Olá,
+            Eu gostaria de ser adicionado na newsletter do site.
+            Meu email é ${email},
+
+            Obrigado!
+            `,
+        }).then(
+          message => alert(message)
+        );
+    })
+});
+
